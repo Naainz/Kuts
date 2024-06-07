b8e880b73c29bfe384079419d060fc72d9c89a2d
Stefan <67121265+Naainz@users.noreply.github.com>
2024-06-07 17:35:48 +0100

    edit default embeds

--- theme.config.tsx
+++ theme.config.tsx
@@ -3,6 +3,13 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 
 
 export default {
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="Kuts.club" />
+      <meta property="og:description" content="The best URL Shortener out there." />
+    </>
+  )
   useNextSeoProps() {
     return {
       titleTemplate: '%s – Kuts.club'
